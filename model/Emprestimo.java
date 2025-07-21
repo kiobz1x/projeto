e518be39f1bf31abba883a21f3b0c199911510c1
Kaio <kaio.bezerra1809@gmail.com>
2025-07-21 16:43:47 -0300

    Update Emprestimo.java

--- model/Emprestimo.java
+++ model/Emprestimo.java
@@ -1,21 +1,28 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Emprestimo {
+    private String id; // esse ID é PAra ajudar a associar as multas
     private Usuario usuario;
     private Obra obra;
     private LocalDate dataEmprestimo;
     private LocalDate dataPrevistaDevolucao;
-    private LocalDate dataDevolucao; // null se ainda não devolvido
+    private LocalDate dataDevolucao; // null se não devolvido
 
     public Emprestimo(Usuario usuario, Obra obra, LocalDate dataEmprestimo) {
+        this.id = UUID.randomUUID().toString(); // aqui ta criando o ID
         this.usuario = usuario;
         this.obra = obra;
         this.dataEmprestimo = dataEmprestimo;
         this.dataPrevistaDevolucao = dataEmprestimo.plusDays(obra.getTempoEmprestimo());
         this.dataDevolucao = null;
-        obra.setEmprestado(true); // marca a obra como emprestada
+        obra.setEmprestado(true); //obra emprestada
+    }
+
+    public String getId() {
+        return id;
     }
 
     public boolean devolver(LocalDate dataDevolucao) {
