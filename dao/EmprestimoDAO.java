2a9bc682316913c874a0ccde4bfd319eeb742b3a
Kaio <kaio.bezerra1809@gmail.com>
2025-07-21 16:41:45 -0300

    Update EmprestimoDAO.java

--- dao/EmprestimoDAO.java
+++ dao/EmprestimoDAO.java
@@ -1,33 +1,54 @@
 package dao;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import model.Emprestimo;
+import model.Obra;
+import model.Livro;
+import model.Revista;
+import model.Artigo;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmprestimoDAO implements Persistivel<Emprestimo> {
     private final String arquivo = "emprestimos.json";
-    private final Gson gson = new Gson();
+    private final Gson gson;
+
+    public EmprestimoDAO() {
+        // Configura o RuntimeTypeAdapterFactory para a classe abstrata Obra
+        RuntimeTypeAdapterFactory<Obra> obraAdapterFactory = RuntimeTypeAdapterFactory
+            .of(Obra.class, "tipo")
+            .registerSubtype(Livro.class, "Livro")
+            .registerSubtype(Revista.class, "Revista")
+            .registerSubtype(Artigo.class, "Artigo");
+
+        this.gson = new GsonBuilder()
+            .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+            .registerTypeAdapterFactory(obraAdapterFactory)
+            .setPrettyPrinting()
+            .create();
+    }
 
     @Override
-    public List<Emprestimo> carregar() { // Agora ele faz List<Emprestimo>
+    public List<Emprestimo> carregar() {
         try (FileReader reader = new FileReader(arquivo)) {
             Type tipoLista = new TypeToken<List<Emprestimo>>() {}.getType();
             List<Emprestimo> lista = gson.fromJson(reader, tipoLista);
             return (lista != null) ? lista : new ArrayList<>();
-        } catch (IOException e) {
+        } catch (IOException | JsonSyntaxException e) {
+            System.out.println("Arquivo não encontrado ou inválido. Iniciando lista vazia.");
             return new ArrayList<>();
         }
     }
 
     @Override
-    public void salvar(List<Emprestimo> lista) { // E grava List<Emprestimo>
+    public void salvar(List<Emprestimo> lista) {
         try (FileWriter writer = new FileWriter(arquivo)) {
             gson.toJson(lista, writer);
         } catch (IOException e) {
