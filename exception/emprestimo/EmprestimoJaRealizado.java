2a02c88973c7cdf9f35d60f2b7f4ec9e999a9fd7
Kaio <kaio.bezerra1809@gmail.com>
2025-07-28 13:44:02 -0300

    Create EmprestimoJaRealizado.java

---
+++ exception/emprestimo/EmprestimoJaRealizado.java
@@ -0,0 +1,7 @@
+package exceptions.emprestimo;
+
+public class EmprestimoJaRealizadoException extends Exception {
+    public EmprestimoJaRealizadoException(String mensagem) {
+        super(mensagem);
+    }
+}
