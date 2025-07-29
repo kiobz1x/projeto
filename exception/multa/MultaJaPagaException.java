e4ee4e749f2ed9ecf0ce7955ae7dd9e0a4d0e217
Marianne-Remigio <mariannelimaremigio7@gmail.com>
2025-07-29 00:28:19 -0300

    Update MultaJaPagaException.java

--- exception/multa/MultaJaPagaException.java
+++ exception/multa/MultaJaPagaException.java
@@ -1,6 +1,7 @@
-package exceptions.multa;
+package exception.multa;
 
 public class MultaJaPagaException   extends Exception {
+	private static final long serialVersionUID = 1L;
     public MultaJaPagaException(String mensagem) {
         super(mensagem);
     }
