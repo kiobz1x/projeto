d6560ed450d011799dd74589045ae3a8b7cad919
Marianne-Remigio <mariannelimaremigio7@gmail.com>
2025-07-29 00:25:43 -0300

    Update DevolucaoInvalidaException.java

--- exception/emprestimo/DevolucaoInvalidaException.java
+++ exception/emprestimo/DevolucaoInvalidaException.java
@@ -1,6 +1,7 @@
-package exceptions.emprestimo;
+package exception.emprestimo;
 
 public class DevolucaoInvalidaException  extends Exception {
+	private static final long serialVersionUID = 1L;
     public DevolucaoInvalidaException(String mensagem) {
         super(mensagem);
     }
