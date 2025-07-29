c8f89e546454dc741cee94652d23c82942f3abbf
Marianne-Remigio <mariannelimaremigio7@gmail.com>
2025-07-29 00:26:19 -0300

    Update EmprestimoNaoEncontradoException.java

--- exception/emprestimo/EmprestimoNaoEncontradoException.java
+++ exception/emprestimo/EmprestimoNaoEncontradoException.java
@@ -1,6 +1,7 @@
-package exceptions.emprestimo;
+package exception.emprestimo;
 
 public class EmprestimoNaoEncontradoException  extends Exception {
+	private static final long serialVersionUID = 1L;
     public EmprestimoNaoEncontradoException(String mensagem) {
         super(mensagem);
     }
