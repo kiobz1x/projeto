34312420075384fad10a2d2ec7be299a3bd42977
Marianne-Remigio <mariannelimaremigio7@gmail.com>
2025-07-29 00:28:46 -0300

    Update MultaNaoEncontradaException.java

--- exception/multa/MultaNaoEncontradaException.java
+++ exception/multa/MultaNaoEncontradaException.java
@@ -1,6 +1,7 @@
-package exceptions.multa;
+package exception.multa;
 
 public class MultaNaoEncontradaException extends Exception {
+	private static final long serialVersionUID = 1L;
     public MultaNaoEncontradaException(String mensagem) {
         super(mensagem);
     }
