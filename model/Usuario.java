dd6f1605c11cc3ef02bb32040ec0e7a4c426ee06
Kaio <kaio.bezerra1809@gmail.com>
2025-07-30 13:15:36 -0300

    Update Usuario.java

--- model/Usuario.java
+++ model/Usuario.java
@@ -1,11 +1,22 @@
 package model;
 
 public class Usuario {
+    private static int contadorMatricula = 1;
+
     private String nome;
-    private String matricula; // deve ser única
+    private String matricula; // único
     private TipoUsuario tipo;
     private String telefone;
     private String email;
+
+    // Construtor com matrícula automática
+    public Usuario(String nome, TipoUsuario tipo, String telefone, String email) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.telefone = telefone;
+        this.email = email;
+        this.matricula = gerarMatricula();
+    }
 
     public Usuario(String nome, String matricula, TipoUsuario tipo, String telefone, String email) {
         this.nome = nome;
@@ -15,7 +26,13 @@ public class Usuario {
         this.email = email;
     }
 
-    // Getters e Setters
+    // formato da matricula padronizada USU-00001
+    private String gerarMatricula() {
+        String novaMatricula = String.format("USU-%05d", contadorMatricula);
+        contadorMatricula++;
+        return novaMatricula;
+    }
+
     public String getNome() {
         return nome;
     }
@@ -39,7 +56,11 @@ public class Usuario {
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    public void setMatricula(String novaMatricula) {
+		this.matricula = novaMatricula;
+		
+	}
+    
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
