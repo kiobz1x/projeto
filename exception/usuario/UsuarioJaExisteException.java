5a7b486ddbf92bc93c332beed97984d027606d2d
Kaio <kaio.bezerra1809@gmail.com>
2025-07-30 13:14:45 -0300

    Update UsuarioJaExisteException.java

--- exception/usuario/UsuarioJaExisteException.java
+++ exception/usuario/UsuarioJaExisteException.java
@@ -1,8 +1,14 @@
-package exception.usuario;
+package exception;
 
-public class UsuarioJaExisteException extends Exception{
-	private static final long serialVersionUID = 1L;
-	public UsuarioJaExisteException() {
+public class UsuarioJaExisteException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public UsuarioJaExisteException() {
         super("⚠️ Usuário já está cadastrado no sistema.");
+    }
+
+    // Construtor que recebe mensagem personalizada
+    public UsuarioJaExisteException(String message) {
+        super(message);
     }
 }
