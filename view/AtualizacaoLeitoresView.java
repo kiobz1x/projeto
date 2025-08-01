7b46a976e874f0cda9cfd7e5edfd149c31afc6a8
Marianne-Remigio <mariannelimaremigio7@gmail.com>
2025-08-01 13:01:52 -0300

    Create AtualizacaoLeitoresView.java

---
+++ view/AtualizacaoLeitoresView.java
@@ -0,0 +1,158 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import controller.LeitorController;
+import model.Leitor;
+import model.TipoLeitor;
+import exception.leitor.LeitorNaoExisteException;
+
+public class AtualizacaoLeitoresView extends JFrame{
+	private final JTextField campoNome;
+    private final JTextField campoTelefone;
+    private final JTextField campoEmail;
+    private final JComboBox<String> comboTipoLeitor;
+    private final JTextField campoMatricula;
+    private final JLabel mensagem;
+
+    public AtualizacaoLeitoresView() {
+        setResizable(false);
+        setTitle("👤 Atualização de dados dos Leitores");
+        setSize(797, 532);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        getContentPane().setLayout(new BorderLayout());
+
+        JPanel painelLeitor = new JPanel();
+        painelLeitor.setBounds(90, 155, 765, 689);
+        painelLeitor.setBorder(BorderFactory.createEmptyBorder(20, 40, 10, 40));
+        painelLeitor.setLayout(null);
+
+        JLabel cl = new JLabel("Atualização de dados dos Leitores");
+        cl.setFont(new Font("Arial", Font.PLAIN, 20));
+        cl.setBounds(231, 50, 321, 24);
+        painelLeitor.add(cl);
+        
+        JLabel matricula = new JLabel("Digite a sua matricula:");
+        matricula.setFont(new Font("Verdana", Font.PLAIN, 14));
+        matricula.setBounds(121, 110, 172, 48);
+        painelLeitor.add(matricula);
+        campoMatricula = new JTextField();
+        campoMatricula.setFont(new Font("Verdana", Font.PLAIN, 14));
+        campoMatricula.setBounds(343, 118, 349, 33);
+        painelLeitor.add(campoMatricula);
+        campoMatricula.setColumns(10);
+
+        JLabel nomeNovo = new JLabel("Nome novo:");
+        nomeNovo.setFont(new Font("Verdana", Font.PLAIN, 14));
+        nomeNovo.setBounds(169, 168, 124, 48);
+        painelLeitor.add(nomeNovo);
+        campoNome = new JTextField();
+        campoNome.setFont(new Font("Verdana", Font.PLAIN, 14));
+        campoNome.setBounds(343, 176, 349, 33);
+        painelLeitor.add(campoNome);
+        campoNome.setColumns(10);
+
+        JLabel telefoneNovo = new JLabel("Telefone novo:");
+        telefoneNovo.setFont(new Font("Verdana", Font.PLAIN, 14));
+        telefoneNovo.setBounds(169, 222, 124, 31);
+        painelLeitor.add(telefoneNovo);
+        campoTelefone = new JTextField();
+        campoTelefone.setFont(new Font("Verdana", Font.PLAIN, 14));
+        campoTelefone.setColumns(10);
+        campoTelefone.setBounds(343, 223, 349, 28);
+        painelLeitor.add(campoTelefone);
+
+        JLabel emailNovo = new JLabel("E-mail novo:");
+        emailNovo.setFont(new Font("Verdana", Font.PLAIN, 14));
+        emailNovo.setBounds(169, 264, 124, 31);
+        painelLeitor.add(emailNovo);
+        campoEmail = new JTextField();
+        campoEmail.setFont(new Font("Verdana", Font.PLAIN, 14));
+        campoEmail.setColumns(10);
+        campoEmail.setBounds(343, 265, 349, 28);
+        painelLeitor.add(campoEmail);
+
+        JLabel tipoNovo = new JLabel("Tipo o novo tipo de leitor:");
+        tipoNovo.setFont(new Font("Verdana", Font.PLAIN, 14));
+        tipoNovo.setBounds(121, 306, 222, 31);
+        painelLeitor.add(tipoNovo);
+        comboTipoLeitor = new JComboBox<>(new String[]{" ", "ALUNO", "PROFESSOR", "SERVIDOR"});
+        comboTipoLeitor.setBounds(343, 303, 212, 31);
+        painelLeitor.add(comboTipoLeitor);
+
+        mensagem = new JLabel("", SwingConstants.CENTER);
+        getContentPane().add(mensagem, BorderLayout.NORTH);
+        getContentPane().add(painelLeitor, BorderLayout.CENTER);
+
+        JButton botaoCadastrar = new JButton("Atualizar Leitor");
+        botaoCadastrar.setFont(new Font("Verdana", Font.PLAIN, 16));
+        botaoCadastrar.setBounds(275, 386, 263, 21);
+        painelLeitor.add(botaoCadastrar);
+        botaoCadastrar.addActionListener(e -> atualizarLeitor());
+
+        setVisible(true);
+    }
+
+    private void atualizarLeitor() {
+        String nomeNovo = campoNome.getText().trim();
+        String matricula = campoMatricula.getText().trim();
+        String telefoneNovo = campoTelefone.getText().trim();
+        String emailNovo = campoEmail.getText().trim();
+        String tipoStr = (String) comboTipoLeitor.getSelectedItem();
+
+        // Validações básicas
+        if (nomeNovo.isEmpty() || matricula.isEmpty() ||telefoneNovo.isEmpty() || emailNovo.isEmpty() || tipoStr == null || tipoStr.isBlank()) {
+            mensagem.setText("⚠ Preencha todos os campos.");
+            return;
+		}
+
+        if (!telefoneNovo.matches("\\d+")) {
+            mensagem.setText("⚠ O telefone deve conter apenas números.");
+            return;
+        }
+
+        if (!emailNovo.matches("^[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,}$")) {
+            mensagem.setText("⚠ E-mail inválido. Ex: exemplo@dominio.com");
+            return;
+        }
+
+        if (!nomeNovo.matches("^[A-Za-zÀ-ÿ ]+$")) {
+            mensagem.setText("⚠ O nome deve conter apenas letras.");
+            return;
+        }
+
+        try {
+            TipoLeitor tipo = TipoLeitor.valueOf(tipoStr);
+            Leitor leitor = new Leitor(nomeNovo, tipo, telefoneNovo, emailNovo); // matrícula automática
+            LeitorController controller = new LeitorController();
+            controller.atualizarLeitor(nomeNovo, tipo ,telefoneNovo, emailNovo);
+            mensagem.setText("✅ Leitor atualizado com sucesso! Matrícula: " + leitor.getMatricula());
+            limparCampos();
+        } catch (IllegalArgumentException e) {
+            mensagem.setText("❌ Tipo de leitor inválido.");
+        } catch (Exception e) {
+            mensagem.setText("❌ Erro inesperado ao cadastrar.");
+            e.printStackTrace();
+        }
+    }
+
+    private void limparCampos() {
+        campoNome.setText("");
+        campoMatricula.setText("");
+        campoTelefone.setText("");
+        campoEmail.setText("");
+        comboTipoLeitor.setSelectedIndex(0);
+    }
+}
+
