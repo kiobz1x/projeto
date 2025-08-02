9a0db397a428b6744ae80b380d2a5d085a39fe15
Marianne-Remigio <mariannelimaremigio7@gmail.com>
2025-08-01 22:59:34 -0300

    Update ObraNaoDisponivelException.java

--- exception/obras/ObraNaoDisponivelException.java
+++ exception/obras/ObraNaoDisponivelException.java
@@ -2,8 +2,7 @@ package exception.obras;
 
 public class ObraNaoDisponivelException extends Exception{
 	private static final long serialVersionUID = 1L;
-	  
-	// ✅ Construtor com mensagem personalizada
+
 	public ObraNaoDisponivelException(String message) {
 		super(message);
 	}
