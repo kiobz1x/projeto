e526e90407d3f45e5253c731071440467c981f83
Marianne-Remigio <mariannelimaremigio7@gmail.com>
2025-08-01 23:08:05 -0300

    Update LeitorJaExisteException.java

--- exception/leitor/LeitorJaExisteException.java
+++ exception/leitor/LeitorJaExisteException.java
@@ -3,6 +3,6 @@ package exception.leitor;
 public class LeitorJaExisteException extends Exception{
 	private static final long serialVersionUID = 1L;
 	public LeitorJaExisteException() {
-        super("⚠️ Leitor já está cadastrado no sistema.");
+        super("Leitor já está cadastrado no sistema.");
 	}
 }
