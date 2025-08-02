92ec8ec4626feac54c26313cae6c6c677b7d7d04
Marianne-Remigio <mariannelimaremigio7@gmail.com>
2025-08-01 23:07:19 -0300

    Update UsuarioJaExisteException.java

--- exception/usuario/UsuarioJaExisteException.java
+++ exception/usuario/UsuarioJaExisteException.java
@@ -1,13 +1,12 @@
-package exception;
+package exception.usuario;
 
 public class UsuarioJaExisteException extends Exception {
     private static final long serialVersionUID = 1L;
 
     public UsuarioJaExisteException() {
-        super("⚠️ Usuário já está cadastrado no sistema.");
+        super("Usuário já está cadastrado no sistema.");
     }
 
-    // Construtor que recebe mensagem personalizada
     public UsuarioJaExisteException(String message) {
         super(message);
     }
